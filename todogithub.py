1ae5f8a0f826bc99766ffd6da9c456e0d0d1b47d
Radeen Mostafa <66905164+RadeenXALNW@users.noreply.github.com>
2024-04-06 16:20:46 +0600

    Add files via upload

---
+++ todogithub.py
@@ -0,0 +1,16 @@
+from crewai import Agent, Task
+from composio_crewai import ComposioToolset, App, Action
+from langchain_openai import ChatOpenAI
+from langchain.agents import AgentType
+llm = ChatOpenAI(openai_api_key="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", model="gpt-4-0613")
+
+composioCrewAI = ComposioToolset([App.GITHUB, App.LINEAR])
+
+agent = Agent(role='Github-Linear TODO Agent',
+  goal="""Take action on Linear via Linear APIs based on Github commits. Linear Project to create issues: Hermes""",
+  backstory="""You are an AI Agent with access to Github and Linear and wants to keep the Github Code TODOs and Linear in Sync. Linear Project to create issues: Hermes""",
+  verbose=True,
+  tools=composioCrewAI,
+  llm=llm)
+
+#TODO: Multiple errors can be wrapped inside an exception.
